2e077caa292c1a320d114aa2c574f28783eded00
Andrea Del Cimmuto <andrea.delcimmuto@polofermigiorgi.it>
2023-03-19 19:29:37 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.rubricagrafica {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.rubricagrafica to javafx.fxml;
+    exports com.example.rubricagrafica;
+}
